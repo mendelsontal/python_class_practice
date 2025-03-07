feccaa7cab2213b49e3210e3ced8663d88a4e66a
baruchgu <baruch.gudesblat@gmail.com>
2025-03-07 11:45:01 +0200

    verify if python3 is installed

--- src/env_verify.sh
+++ src/env_verify.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 ############################################################
 # Developed by: Tal & Baruch 
-# Description:
+# Description: Verify if python3 is installed
 # Date:07/03/2025                                                                                                                                   #
-# Version: 0.0.1
+# Version: 1.0.0
 ############################################################
+
+[[ $(which python3s) == "" ]] && printf "Error: Python3 is not installed.\nSuggestion: sudo apt-get install python3\n"
