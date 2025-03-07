f7dccc4190da69313e755f4090c4d8f9315bf913
baruchgu <baruch.gudesblat@gmail.com>
2025-03-07 10:15:43 +0200

    add .gitignore app.py

---
+++ src/srv/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+############################################################
+# Developed by: Tal & Baruch 
+# Description:
+# Date:07/03/2025                                                                                                                                   #
+# Version: 0.0.1
+############################################################
+
+import os
+import sys
+import time
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from src.lib import lib
+
+user_name = input('[?] Please provide name, habibi: ')
+
+hello_user_name_value = lib.hello_name(user_name)
+goodbye_user_name_value = lib.goodbye_name(user_name)
+
+print(f'[+] {hello_user_name_value}')
+time.sleep(2)
+print('[+] Running computing jobs')
+time.sleep(2)
+print('[+] Still computing')
+time.sleep(2)
+print('[+] Computations completed successfully')
+time.sleep(2)
+print(f'[+] {goodbye_user_name_value}')
